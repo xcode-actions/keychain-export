711dd5c1b8f3d44dea0a52da9c6140d4ded1a829
Frizlab <fload@me.com>
2024-02-11 18:56:46 +0100

    Implement the most important part of the tool
    Retrieving an encrypted PEM containing the private key works.

--- Package.swift
+++ Package.swift
@@ -1,21 +1,23 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
+
 let package = Package(
-    name: "keychain-export",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "keychain-export",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
-        ),
-    ]
+	name: "keychain-export",
+	platforms: [.macOS(.v11)],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+		.package(url: "https://github.com/apple/swift-log.git",             from: "1.5.4"),
+		.package(url: "https://github.com/xcode-actions/clt-logger.git",    from: "0.8.0"),
+	],
+	targets: [
+		.executableTarget(
+			name: "keychain-export",
+			dependencies: [
+				.product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "CLTLogger",      package: "clt-logger"),
+				.product(name: "Logging",        package: "swift-log"),
+			]
+		),
+	]
 )
---
+++ Sources/keychain-export.swift
@@ -0,0 +1,148 @@
+import Foundation
+import Security
+
+import ArgumentParser
+
+
+
+@main
+struct KeychainExport : AsyncParsableCommand {
+	
+//	@Option
+//	var entityType: EntityType = .certificate
+	
+	@Argument
+	var entityName: String
+	
+	func run() async throws {
+		let certificate = try findCertificate(named: entityName)
+		let identity = try findIdentity(matching: certificate)
+		
+		let privateKey = try {
+			var res: SecKey?
+			let err = SecIdentityCopyPrivateKey(identity, &res)
+			guard err == noErr else {throw Self.secErrorFrom(statusCode: err)}
+			return res!
+		}()
+		let privateKeyData = try {
+			let password = "42toto42" as CFTypeRef
+			let alertTitle = "BEWARE!" as CFString
+			let alertPrompt = "You’re exporting a private key, you fool." as CFString
+			var keyParams = SecItemImportExportKeyParameters(
+				version: UInt32(SEC_KEY_IMPORT_EXPORT_PARAMS_VERSION),
+				flags: [/*.securePassphrase*/],
+				passphrase: Unmanaged.passUnretained(password),
+				alertTitle: Unmanaged.passUnretained(alertTitle),
+				alertPrompt: Unmanaged.passUnretained(alertPrompt),
+				accessRef: nil, keyUsage: nil, keyAttributes: nil
+			)
+			var res: CFData?
+			let err = SecItemExport(privateKey, .formatWrappedPKCS8, .pemArmour, &keyParams, &res)
+			guard err == noErr else {throw Self.secErrorFrom(statusCode: err)}
+			return res! as Data
+			/* The following does not work.
+			 * There might be a way to make it work, but I don’t know it. */
+//			var error: Unmanaged<CFError>?
+//			guard let data = SecKeyCopyExternalRepresentation(privateKey, &error) else {
+//				throw SimpleError("Failed copying private key content: \((error?.takeUnretainedValue()).flatMap{ "\($0)" } ?? "Unknown error").")
+//			}
+//			return data as NSData
+		}()
+		
+		guard let privateKeyString = String(data: privateKeyData, encoding: .ascii) else {
+			throw SimpleError("Cannot read private key data as PEM.")
+		}
+		print(privateKeyString)
+	}
+	
+	private func findIdentity(matching certificate: SecCertificate) throws -> SecIdentity {
+		var query = [String: Any]()
+		query[kSecClass               as String] = kSecClassIdentity
+		query[kSecMatchItemList       as String] = [certificate] as CFArray
+		query[kSecMatchLimit          as String] = kSecMatchLimitAll
+		query[kSecReturnData          as String] = kCFBooleanFalse
+		query[kSecReturnRef           as String] = kCFBooleanTrue
+		query[kSecReturnPersistentRef as String] = kCFBooleanFalse
+		query[kSecReturnAttributes    as String] = kCFBooleanFalse
+		
+		var searchResult: CFTypeRef?
+		let error = SecItemCopyMatching(query as CFDictionary, &searchResult)
+		switch error {
+			case errSecSuccess:
+				guard CFGetTypeID(searchResult) == CFArrayGetTypeID() else {
+					throw SimpleError("Result does not have the expected type.")
+				}
+				let resultArray = searchResult as! [CFTypeRef]
+				let c = resultArray.count
+				guard c == 1 else {
+					throw SimpleError("Got \(c) results; expected only 1.")
+				}
+				let firstResult = resultArray[0]
+				guard CFGetTypeID(firstResult) == SecIdentityGetTypeID() else {
+					throw SimpleError("Result does not have the expected type.")
+				}
+				return firstResult as! SecIdentity
+				
+			case errSecItemNotFound:
+				throw SimpleError("No certificate with the given name.")
+				
+			default:
+				throw Self.secErrorFrom(statusCode: error)
+		}
+	}
+	
+	private func findCertificate(named: String) throws -> SecCertificate {
+		var query = [String: Any]()
+		query[kSecClass               as String] = kSecClassCertificate
+		query[kSecAttrLabel           as String] = entityName
+		query[kSecMatchLimit          as String] = kSecMatchLimitAll
+		query[kSecReturnData          as String] = kCFBooleanFalse
+		query[kSecReturnRef           as String] = kCFBooleanTrue
+		query[kSecReturnPersistentRef as String] = kCFBooleanFalse
+		query[kSecReturnAttributes    as String] = kCFBooleanFalse
+		
+		var searchResult: CFTypeRef?
+		let error = SecItemCopyMatching(query as CFDictionary, &searchResult)
+		switch error {
+			case errSecSuccess:
+				guard CFGetTypeID(searchResult) == CFArrayGetTypeID() else {
+					throw SimpleError("Result does not have the expected type.")
+				}
+				let resultArray = searchResult as! [CFTypeRef]
+				let c = resultArray.count
+				guard c == 1 else {
+					throw SimpleError("Got \(c) results; expected only 1.")
+				}
+				let firstResult = resultArray[0]
+				guard CFGetTypeID(firstResult) == SecCertificateGetTypeID() else {
+					throw SimpleError("Result does not have the expected type.")
+				}
+				return firstResult as! SecCertificate
+				
+			case errSecItemNotFound:
+				throw SimpleError("No certificate with the given name.")
+				
+			default:
+				throw Self.secErrorFrom(statusCode: error)
+		}
+	}
+	
+	private static func secErrorFrom(statusCode: OSStatus) -> SimpleError {
+#if os(macOS)
+		return SimpleError("Security framework failure \(statusCode): \(SecCopyErrorMessageString(statusCode, nil /* reserved for future use */) as String?).")
+#else
+		return SimpleError("Security framework failure \(statusCode)")
+#endif
+	}
+	
+}
+
+
+struct SimpleError : Error {
+	
+	var message: String
+	init(_ message: String) {
+		self.message = message
+	}
+	
+}
