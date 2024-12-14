ed6e3bbb00bf501582c660d059314cadb6f35e71
Frizlab <fload@me.com>
2024-12-14 13:30:28 +0100

    Add an option to skip printing the private key

--- Sources/keychain-export.swift
+++ Sources/keychain-export.swift
@@ -16,13 +16,15 @@ struct KeychainExport : AsyncParsableCommand {
 	
 	@Option
 	var skipCertificate: Bool = false
+	@Option
+	var skipPrivateKey: Bool = false
 	
 	@Argument
 	var entityName: String
 	
 	func run() async throws {
+		/* The certificate is needed to find the private key, so we cannot skip searching for it. */
 		let certificate = try findCertificate(named: entityName)
-		let identity = try findIdentity(matching: certificate)
 		
 		let certificateString: String?
 		if !skipCertificate {
@@ -57,61 +59,65 @@ struct KeychainExport : AsyncParsableCommand {
 			certificateString = nil
 		}
 		
-		let privateKey = try {
-			var res: SecKey?
-			let err = SecIdentityCopyPrivateKey(identity, &res)
-			guard err == noErr else {throw Self.secErrorFrom(statusCode: err)}
-			return res!
-		}()
-		let privateKeyData = try {
-			let password = Self.dummyPassword as CFTypeRef
-			let alertTitle = "BEWARE!" as CFString
-			let alertPrompt = "You’re exporting a private key, you fool." as CFString
-			var keyParams = SecItemImportExportKeyParameters(
-				version: UInt32(SEC_KEY_IMPORT_EXPORT_PARAMS_VERSION),
-				flags: [/*.securePassphrase*/],
-				passphrase: Unmanaged.passUnretained(password),
-				alertTitle: Unmanaged.passUnretained(alertTitle),
-				alertPrompt: Unmanaged.passUnretained(alertPrompt),
-				accessRef: nil, keyUsage: nil, keyAttributes: nil
-			)
-			var res: CFData?
-			let err = SecItemExport(privateKey, .formatWrappedPKCS8, .pemArmour, &keyParams, &res)
-			guard err == noErr else {throw Self.secErrorFrom(statusCode: err)}
-			return res! as Data
-			/* The following does not work.
-			 * There might be a way to make it work, but I don’t know it. */
-//			var error: Unmanaged<CFError>?
-//			guard let data = SecKeyCopyExternalRepresentation(privateKey, &error) else {
-//				throw SimpleError("Failed copying private key content: \((error?.takeUnretainedValue()).flatMap{ "\($0)" } ?? "Unknown error").")
-//			}
-//			return data as NSData
-		}()
+		let decodedPrivateKeyString: String?
+		if !skipPrivateKey {
+			let identity = try findIdentity(matching: certificate)
+			let privateKey = try {
+				var res: SecKey?
+				let err = SecIdentityCopyPrivateKey(identity, &res)
+				guard err == noErr else {throw Self.secErrorFrom(statusCode: err)}
+				return res!
+			}()
+			let privateKeyData = try {
+				let password = Self.dummyPassword as CFTypeRef
+				let alertTitle = "BEWARE!" as CFString
+				let alertPrompt = "You’re exporting a private key, you fool." as CFString
+				var keyParams = SecItemImportExportKeyParameters(
+					version: UInt32(SEC_KEY_IMPORT_EXPORT_PARAMS_VERSION),
+					flags: [/*.securePassphrase*/],
+					passphrase: Unmanaged.passUnretained(password),
+					alertTitle: Unmanaged.passUnretained(alertTitle),
+					alertPrompt: Unmanaged.passUnretained(alertPrompt),
+					accessRef: nil, keyUsage: nil, keyAttributes: nil
+				)
+				var res: CFData?
+				let err = SecItemExport(privateKey, .formatWrappedPKCS8, .pemArmour, &keyParams, &res)
+				guard err == noErr else {throw Self.secErrorFrom(statusCode: err)}
+				return res! as Data
+				/* The following does not work.
+				 * There might be a way to make it work, but I don’t know it. */
+//				var error: Unmanaged<CFError>?
+//				guard let data = SecKeyCopyExternalRepresentation(privateKey, &error) else {
+//					throw SimpleError("Failed copying private key content: \((error?.takeUnretainedValue()).flatMap{ "\($0)" } ?? "Unknown error").")
+//				}
+//				return data as NSData
+			}()
+			
+			/* Use OpenSSL to export the unencrypted private key. */
+			let b = try BIO_new(BIO_s_mem() ?! SimpleError("Cannot allocation BIO mem.")) ?! SimpleError("Cannot create a BIO.")
+			defer {BIO_free(b)}
+			guard (privateKeyData.withUnsafeBytes{ bytes in BIO_write(b, bytes.baseAddress!, Int32(bytes.count)) }) == privateKeyData.count else {
+				throw SimpleError("Failed to write all the data to BIO.")
+			}
+			var password = Self.dummyPassword.utf8CString
+			guard let pkey = (password.withUnsafeMutableBytes{ bytes in PEM_read_bio_PrivateKey(b, nil, nil, bytes.baseAddress!) }) else {
+				throw SimpleError("Cannot read PEM from BIO.")
+			}
+			defer {EVP_PKEY_free(pkey)}
+			let bo = try BIO_new(BIO_s_mem() ?! SimpleError("Cannot allocation BIO mem.")) ?! SimpleError("Cannot create a BIO.")
+			defer {BIO_free(bo)}
+			guard PEM_write_bio_PKCS8PrivateKey(bo, pkey, nil, nil, 0, nil, nil) != 0 else {
+				throw SimpleError("Cannot write PEM to BIO.")
+			}
+			let reader = GenericStreamReader(stream: OpenSSLBio(bio: bo), bufferSize: 1024, bufferSizeIncrement: 512)
+			let decodedPrivateKeyData = try reader.readDataToEnd()
+			decodedPrivateKeyString = try String(data: decodedPrivateKeyData, encoding: .ascii) ?! SimpleError("Cannot decode decoded pkey as ascii String.")
+		} else {
+			decodedPrivateKeyString = nil
+		}
 		
-		/* Use OpenSSL to export the unencrypted private key. */
-		let b = try BIO_new(BIO_s_mem() ?! SimpleError("Cannot allocation BIO mem.")) ?! SimpleError("Cannot create a BIO.")
-		defer {BIO_free(b)}
-		guard (privateKeyData.withUnsafeBytes{ bytes in BIO_write(b, bytes.baseAddress!, Int32(bytes.count)) }) == privateKeyData.count else {
-			throw SimpleError("Failed to write all the data to BIO.")
-		}
-		var password = Self.dummyPassword.utf8CString
-		guard let pkey = (password.withUnsafeMutableBytes{ bytes in PEM_read_bio_PrivateKey(b, nil, nil, bytes.baseAddress!) }) else {
-			throw SimpleError("Cannot read PEM from BIO.")
-		}
-		defer {EVP_PKEY_free(pkey)}
-		let bo = try BIO_new(BIO_s_mem() ?! SimpleError("Cannot allocation BIO mem.")) ?! SimpleError("Cannot create a BIO.")
-		defer {BIO_free(bo)}
-		guard PEM_write_bio_PKCS8PrivateKey(bo, pkey, nil, nil, 0, nil, nil) != 0 else {
-			throw SimpleError("Cannot write PEM to BIO.")
-		}
-		let reader = GenericStreamReader(stream: OpenSSLBio(bio: bo), bufferSize: 1024, bufferSizeIncrement: 512)
-		let decodedPrivateKeyData = try reader.readDataToEnd()
-		let decodedPrivateKeyString = try String(data: decodedPrivateKeyData, encoding: .ascii) ?! SimpleError("Cannot decode decoded pkey as ascii String.")
-		
-		if let certificateString {
-			print(certificateString)
-		}
-		print(decodedPrivateKeyString)
+		if let certificateString       {print(certificateString)}
+		if let decodedPrivateKeyString {print(decodedPrivateKeyString)}
 	}
 	
 	private func findIdentity(matching certificate: SecCertificate) throws -> SecIdentity {
